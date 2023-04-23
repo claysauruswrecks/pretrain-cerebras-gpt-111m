99ba0cb14c1808ea24dfba4eec68320eaf398792
claysauruswrecks <44852834+claysauruswrecks@users.noreply.github.com>
2023-04-23 00:03:15 -0700

    updates to use smaller dataset

--- pretrain-cerebras-111m.py
+++ pretrain-cerebras-111m.py
@@ -13,7 +13,6 @@ from transformers import (
 
 # Step 1: Load the model and the tokenizer
 def load_model_and_tokenizer():
-    # Load the AutoTokenizer and AutoModelForCausalLM to work with the Cerebras-GPT-111M model
     tokenizer = AutoTokenizer.from_pretrained("cerebras/Cerebras-GPT-111M")
     model = AutoModelForCausalLM.from_pretrained("cerebras/Cerebras-GPT-111M")
     return tokenizer, model
@@ -21,33 +20,47 @@ def load_model_and_tokenizer():
 
 # Step 2: Load the dataset and tokenize its texts
 def load_and_tokenize_dataset(tokenizer):
-    # Load the wikitext dataset and tokenize its text using the provided tokenizer
-    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    # dataset = load_dataset("bigcode/the-stack-smol-xl", split="train")
+    dataset = load_dataset("bigcode/the-stack-smol", split="train")
     tokenized_dataset = dataset.map(
-        lambda example: tokenizer(example["text"]),
+        lambda example: tokenizer(example["content"]),
         batched=True,
-        remove_columns=["text"],
+        remove_columns=["content"],
     )
     return tokenized_dataset
 
 
-# Step 3: Create a DataLoader to handle batches during training
+# Updated to handle the validation and test datasets
+def load_and_tokenize_test_and_validation_datasets(tokenizer):
+    validation_dataset = load_dataset("bigcode/the-stack-smol-xl", split="validation")
+    test_dataset = load_dataset("bigcode/the-stack-smol-xl", split="test")
+
+    tokenized_validation_dataset = validation_dataset.map(
+        lambda example: tokenizer(example["content"]),
+        batched=True,
+        remove_columns=["content"],
+    )
+
+    tokenized_test_dataset = test_dataset.map(
+        lambda example: tokenizer(example["content"]),
+        batched=True,
+        remove_columns=["content"],
+    )
+
+    return tokenized_validation_dataset, tokenized_test_dataset
+
+
 def create_data_loader(tokenized_dataset, tokenizer, batch_size=8):
-    # Initialize a DataCollatorForLanguageModeling with the given tokenizer and no masking
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False, pad_to_multiple_of=8
     )
-
-    # Create a DataLoader with the tokenized dataset and the data collator to generate batches
     data_loader = DataLoader(
         tokenized_dataset, shuffle=True, batch_size=batch_size, collate_fn=data_collator
     )
     return data_loader
 
 
-# Step 4: Configure the training process
 def configure_training(output_dir="./pretrained_cerebras/"):
-    # Set up the training arguments with output directory, epochs, batch size, etc.
     training_args = TrainingArguments(
         output_dir=output_dir,
         overwrite_output_dir=True,
@@ -60,59 +73,58 @@ def configure_training(output_dir="./pretrained_cerebras/"):
     return training_args
 
 
-# Step 5: Create a Trainer instance with the provided configurations
-def create_trainer(model, tokenizer, tokenized_dataset, training_args):
-    # Initialize a DataCollatorForLanguageModeling with the given tokenizer for training
+# Updated to include tokenized_validation_dataset in Trainer
+def create_trainer(
+    model, tokenizer, tokenized_dataset, tokenized_validation_dataset, training_args
+):
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False, pad_to_multiple_of=8
     )
 
-    # Create a Trainer with the configurations, model, data collator, dataset, and tokenizer
     trainer = Trainer(
         model=model,
         args=training_args,
         data_collator=data_collator,
         train_dataset=tokenized_dataset,
+        eval_dataset=tokenized_validation_dataset,
         tokenizer=tokenizer,
     )
     return trainer
 
 
-# Step 6: Quantize the model to reduce its size and improve inference performance
 def quantize_model(
     model: torch.nn.Module, example_input: torch.Tensor
 ) -> torch.nn.Module:
-    # Perform dynamic quantization on the model, specifying the layers to quantize
     quantized_model = torch.quantization.quantize_dynamic(
         model, {torch.nn.Linear}, dtype=torch.qint8
     )
     return quantized_model
 
 
-# Step 7: Main function to run the full process
 def main():
-    # Load the model and tokenizer, prepare the dataset and training configurations
     tokenizer, model = load_model_and_tokenizer()
     tokenized_dataset = load_and_tokenize_dataset(tokenizer)
+    (
+        tokenized_validation_dataset,
+        tokenized_test_dataset,
+    ) = load_and_tokenize_test_and_validation_datasets(tokenizer)
     training_args = configure_training()
 
-    # Create a Trainer instance and perform the training
-    trainer = create_trainer(model, tokenizer, tokenized_dataset, training_args)
+    trainer = create_trainer(
+        model, tokenizer, tokenized_dataset, tokenized_validation_dataset, training_args
+    )
     trainer.train()
 
-    # Save the original trained model
     trainer.save_model("./pretrained_cerebras/")
 
-    # Quantize the trained model for better inference performance
-    example_input = tokenized_dataset[0][
-        "input_ids"
-    ]  # Get an example input from the dataset
+    example_input = tokenized_dataset[0]["input_ids"]
     quantized_model = quantize_model(model, example_input)
 
-    # Save the quantized model
     torch.save(quantized_model.state_dict(), "./pretrained_cerebras/quantized-model.pt")
 
-    # Example usage of the model with Hugging Face Pipelines
+    test_results = trainer.evaluate(tokenized_test_dataset)
+    print("Test Results:", test_results)
+
     text = "Generative AI is "
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
     generated_text = pipe(text, max_length=50, do_sample=False, no_repeat_ngram_size=2)[
@@ -120,7 +132,6 @@ def main():
     ]
     print(generated_text["generated_text"])
 
-    # Example usage of the model with its `generate()` method
     inputs = tokenizer(text, return_tensors="pt")
     outputs = model.generate(
         **inputs,
