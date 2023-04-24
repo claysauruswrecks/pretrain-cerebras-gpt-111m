6ddb7db953d0b26f4d9c5eca7532ed3af5aedcb8
claysauruswrecks <44852834+claysauruswrecks@users.noreply.github.com>
2023-04-23 17:04:51 -0700

    no more errors before oom

--- pretrain-cerebras-111m.py
+++ pretrain-cerebras-111m.py
@@ -13,54 +13,79 @@ from transformers import (
 
 # Step 1: Load the model and the tokenizer
 def load_model_and_tokenizer():
+    # Load the tokenizer and the model for GPT-111M
     tokenizer = AutoTokenizer.from_pretrained("cerebras/Cerebras-GPT-111M")
     model = AutoModelForCausalLM.from_pretrained("cerebras/Cerebras-GPT-111M")
+
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
     return tokenizer, model
 
 
 # Step 2: Load the dataset and tokenize its texts
-def load_and_tokenize_dataset(tokenizer):
-    # dataset = load_dataset("bigcode/the-stack-smol-xl", split="train")
+def load_dataset_without_tokenization():
+    # Load the dataset with the specified split
     dataset = load_dataset("bigcode/the-stack-smol", split="train")
-    tokenized_dataset = dataset.map(
-        lambda example: tokenizer(example["content"]),
+    return dataset
+
+
+def create_validation_and_test_splits(
+    raw_dataset, tokenizer, val_ratio=0.1, test_ratio=0.1, max_sequence_length=2048
+):
+    val_size = int(len(raw_dataset) * val_ratio)
+    test_size = int(len(raw_dataset) * test_ratio)
+
+    # Shuffle the raw dataset
+    raw_dataset = raw_dataset.shuffle(seed=42)
+
+    # Get validation, test, and the remaining train dataset
+    val_dataset = raw_dataset.select(range(val_size))
+    test_dataset = raw_dataset.select(range(val_size, val_size + test_size))
+    train_dataset = raw_dataset.select(range(val_size + test_size, len(raw_dataset)))
+
+    # Tokenize the train, validation, and test dataset
+    tokenized_train_dataset = train_dataset.map(
+        lambda x: tokenizer(
+            x["content"], truncation=True, padding=False, max_length=max_sequence_length
+        ),
         batched=True,
         remove_columns=["content"],
     )
-    return tokenized_dataset
-
-
-# Updated to handle the validation and test datasets
-def load_and_tokenize_test_and_validation_datasets(tokenizer):
-    validation_dataset = load_dataset("bigcode/the-stack-smol-xl", split="validation")
-    test_dataset = load_dataset("bigcode/the-stack-smol-xl", split="test")
-
-    tokenized_validation_dataset = validation_dataset.map(
-        lambda example: tokenizer(example["content"]),
+    tokenized_val_dataset = val_dataset.map(
+        lambda x: tokenizer(
+            x["content"], truncation=True, padding=False, max_length=max_sequence_length
+        ),
         batched=True,
         remove_columns=["content"],
     )
-
     tokenized_test_dataset = test_dataset.map(
-        lambda example: tokenizer(example["content"]),
+        lambda x: tokenizer(
+            x["content"], truncation=True, padding=False, max_length=max_sequence_length
+        ),
         batched=True,
         remove_columns=["content"],
     )
 
-    return tokenized_validation_dataset, tokenized_test_dataset
+    return tokenized_train_dataset, tokenized_val_dataset, tokenized_test_dataset
 
 
 def create_data_loader(tokenized_dataset, tokenizer, batch_size=8):
+    # Create a data collator for language modeling
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False, pad_to_multiple_of=8
     )
+
+    # Create a DataLoader with the specified batch size and data collator
     data_loader = DataLoader(
         tokenized_dataset, shuffle=True, batch_size=batch_size, collate_fn=data_collator
     )
+
     return data_loader
 
 
 def configure_training(output_dir="./pretrained_cerebras/"):
+    # Configure the training arguments
     training_args = TrainingArguments(
         output_dir=output_dir,
         overwrite_output_dir=True,
@@ -77,10 +102,12 @@ def configure_training(output_dir="./pretrained_cerebras/"):
 def create_trainer(
     model, tokenizer, tokenized_dataset, tokenized_validation_dataset, training_args
 ):
+    # Create a data collator for language modeling
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False, pad_to_multiple_of=8
     )
 
+    # Create a Trainer with the specified model, training arguments, datasets and tokenizer
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -95,6 +122,7 @@ def create_trainer(
 def quantize_model(
     model: torch.nn.Module, example_input: torch.Tensor
 ) -> torch.nn.Module:
+    # Quantize the model
     quantized_model = torch.quantization.quantize_dynamic(
         model, {torch.nn.Linear}, dtype=torch.qint8
     )
@@ -103,21 +131,23 @@ def quantize_model(
 
 def main():
     tokenizer, model = load_model_and_tokenizer()
-    tokenized_dataset = load_and_tokenize_dataset(tokenizer)
+    raw_dataset = load_dataset_without_tokenization()
     (
-        tokenized_validation_dataset,
+        tokenized_train_dataset,
+        tokenized_val_dataset,
         tokenized_test_dataset,
-    ) = load_and_tokenize_test_and_validation_datasets(tokenizer)
+    ) = create_validation_and_test_splits(raw_dataset, tokenizer)
+
     training_args = configure_training()
 
     trainer = create_trainer(
-        model, tokenizer, tokenized_dataset, tokenized_validation_dataset, training_args
+        model, tokenizer, tokenized_train_dataset, tokenized_val_dataset, training_args
     )
     trainer.train()
 
     trainer.save_model("./pretrained_cerebras/")
 
-    example_input = tokenized_dataset[0]["input_ids"]
+    example_input = tokenized_train_dataset[0]["input_ids"]
     quantized_model = quantize_model(model, example_input)
 
     torch.save(quantized_model.state_dict(), "./pretrained_cerebras/quantized-model.pt")
